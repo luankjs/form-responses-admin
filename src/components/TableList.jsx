34afd08a7c4d9d95cdb58ee83b4cf8df155221c8
Luan Santos <luan@tootz.com.br>
2022-08-20 12:22:09 -0300

    fix: add key to some child components

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -20,8 +20,8 @@ const TableList = ({ items, fields, handleOpenResponse }) => {
                 {field.label}
               </th>
             ))}
-            <th scope="col" className="py-3 px-6">
-              <span className="sr-only">Edit</span>
+            <th key="action" scope="col" className="py-3 px-6">
+              <span className="sr-only">Ver detalhes</span>
             </th>
           </tr>
         </thead>
@@ -32,14 +32,17 @@ const TableList = ({ items, fields, handleOpenResponse }) => {
                 field.main ? (
                   <th
                     scope="row"
+                    key={field.name}
                     className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
                     {getItemValue(item, field)}
                   </th>
                 ) : (
-                  <td className="py-4 px-6">{getItemValue(item, field)}</td>
+                  <td key={field.name} className="py-4 px-6">
+                    {getItemValue(item, field)}
+                  </td>
                 )
               )}
-              <td className="py-4 px-6 text-right">
+              <td key="action" className="py-4 px-6 text-right">
                 <button
                   type="button"
                   onClick={() => handleOpenResponse(item)}
